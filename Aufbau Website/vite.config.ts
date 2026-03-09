c92592cfd9da1b8b2fb32df0df74f01c6751da77
lilykath <76181186+lilykath@users.noreply.github.com>
2026-03-09 17:34:23 +0100

    Update vite.config.ts

--- Aufbau Website/vite.config.ts
+++ Aufbau Website/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/HousingPorject/',
+  base: '/HousingPorject/', // changed to my repo name
   plugins: [
     react(),
     tailwindcss(),
